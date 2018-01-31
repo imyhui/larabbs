d6b26dbbf57175d9ef3eef99f90dbeb9fb91dc12
liuyonghui <1359158019@qq.com>
2018-02-01 04:14:16 +0800

    User can create topic

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: andyhui
- * Date: 18-1-19
- * Time: 下午9:50
- */
+<div class="panel panel-default">
+    <div class="panel-body">
+        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 新建帖子
+        </a>
+    </div>
+</div>
